368dd3860b7ec55141d4dfad225a2f39a7c62a96
Feyaz Baker <feyazabubaker@gmail.com>
2020-04-27 14:07:42 +0530

    Switching to msyql Make a new user called dbms_proj_user, with password as 'password', and make a database called dbms_project. Follow this: https://www.digitalocean.com/community/tutorials/how-to-use-mysql-or-mariadb-with-your-django-application-on-ubuntu-14-04

--- dbms/settings.py
+++ dbms/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'dbms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbms_project',
+        'USER': 'dbms_proj_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
