d87d2b0c81bf6efb5480a080507ddf9dc67b107c
Niharrai <50584781+Niharrai@users.noreply.github.com>
2020-06-16 22:17:34 +0530

    .

--- dbms/settings.py
+++ dbms/settings.py
@@ -78,13 +78,15 @@ WSGI_APPLICATION = 'dbms.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+     #   'ENGINE': 'django.db.backends.mysql',
+      # 'USER': 'dbms_proj_user',
+        #HOST': 'localhost',
+        #'PORT': '',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbms_project',
-        'USER': 'dbms_proj_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
